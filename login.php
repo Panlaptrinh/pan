f6e58c4ae37158f00f8980fa821ce0b48d0f0531
Panlaptrinh <128304309+Panlaptrinh@users.noreply.github.com>
2024-04-16 12:12:13 +0700

    aaa

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+  // Xử lý các công việc liên quan đến đăng nhập
+  // ...
+
+  // Hiển thị giao diện đăng nhập
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Đăng nhập</title>
+</head>
+<body>
+  <h1>Đăng nhập</h1>
+  <!-- Giao diện đăng nhập -->
+  <form action="process_login.php" method="POST">
+    <!-- Một số trường nhập liệu và nút đăng nhập -->
+  </form>
+</body>
+</html>
